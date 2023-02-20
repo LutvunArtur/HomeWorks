14fe79e4342e8ece5f3eb601a6edce939006fc7b
HP <lutvun.artur1@gmail.com>
2023-02-20 17:24:48 +0200

    Зробив як зміг, хотів трішки більше пофантазувати з діями але не зовсім зрозумів як правильно їх написати тому залишив як є.

--- hw3.py
+++ hw3.py
@@ -1,27 +1,30 @@
 def summa(a, b):
-    r = a + b
-    print("Reasult=", r)
-def min(a, b) :
-    r = a - b
-    print("Reasult=", r)
-def dil(a, b) :
-    r = a / b
-    print("Reasult=", r)
-def mn(a, b) :
-    r = a * b
-    print("Reasult=", r)
-try :
+    return a + b
+
+def minus(a, b):
+    return a - b
+
+
+def dil(a, b):
+    return a / b
+
+
+def mn(a, b):
+    return a * b
+
+
+try:
     num_1 = float(input("Enter first num :"))
     num_2 = float(input("Enter second num:"))
-    func = int(input("What opperation do u want to do ? \n 1 '+' \n 2 '-' \n 3 '*' \n 4 '/' \n Your Answer"))
-    print("Sorry invalid request, please try again :<")
+    func = int(input("What opperation do u want to do ? \n 1 '+' \n 2 '-' \n 3 '*' \n 4 '/' \n Your Answer:"))
     if func == 1:
-        summa(num_1, num_2)
+        res = summa(num_1, num_2)
     if func == 2:
-        min(num_1, num_2)
+        res = minus(num_1, num_2)
     if func == 3:
-        mn(num_1, num_2)
+        res = mn(num_1, num_2)
     if func == 4:
-        dil(num_1, num_2)
-except ValueError:
+        res = dil(num_1, num_2)
+    print("Reasult = ", res)
+except (ValueError, ZeroDivisionError, NameError):
     print("Sorry invalid request, please try again :<")
