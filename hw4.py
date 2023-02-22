5b6c764cea1f5caf7ed45fd73fce01ab69010c3a
HP <lutvun.artur1@gmail.com>
2023-02-23 00:45:59 +0200

    Треба виправити перше завдання

---
+++ hw4.py
@@ -0,0 +1,45 @@
+########    1    #################
+
+
+val = 2
+new_val = 222
+new_val = (val/2) if new_val < 100 else new_val == -abs(new_val)
+print(new_val)
+
+
+################## 2 ############
+
+
+value = 122
+new_value = "1" if value < 100 else new_val == "0"
+print(int(new_value))
+
+
+##############  3 ##############
+
+
+vali = 122
+new_wali = 872
+new_wali = True if vali < 100 else new_wali == False
+print(new_wali)
+
+
+############# 4 #############
+
+
+my_str = "qwer"
+if len(my_str) < 5:
+    print(my_str + my_str[::])
+else:
+    print(my_str)
+
+
+################ 5 ###############
+
+
+my_str = "qwer"
+if len(my_str) < 5:
+    print(my_str + my_str[::-1])
+else:
+    print(my_str)
+
