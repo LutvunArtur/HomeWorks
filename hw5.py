6d4849976ecfce9c7e063a4fad214827070208da
HP <lutvun.artur1@gmail.com>
2023-02-27 22:47:51 +0200

    Треба доробити

---
+++ hw5.py
@@ -0,0 +1,33 @@
+line = "Hello world"
+
+########  a ##############
+
+
+print(line[3])
+
+
+######## b ################
+
+
+print(line[-2])
+
+######### c ############
+
+
+print(line[:5])
+
+
+############## d ##########
+
+
+print(line[:9])
+
+
+########### e #############
+
+
+for lll in line:
+    if lll.isnumeric():
+        lll = line.index()
+        print(lll)
+
